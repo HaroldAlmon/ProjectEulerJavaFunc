a4fbe3961880cd02162a25d9190e1265502f0516
haroldalmon <HaroldAlmon@users.noreply.github.com>
2016-02-10 14:55:29 -0600

    Refactor. Delete function.

--- src/main/java/com/translationdata/p010/P010_SummationOfPrimes.java
+++ src/main/java/com/translationdata/p010/P010_SummationOfPrimes.java
@@ -1,5 +1,5 @@
 package com.translationdata.p010;
-/** Strategy: Brute Force. */
+/** Strategy: Sieve of SieveOfEratosthenes. */
 import static org.junit.Assert.assertEquals;
 
 import java.util.stream.LongStream;
@@ -12,12 +12,12 @@ import JUnitTests.FastTest;
 import misc.SieveOfEratosthenes;
 public class P010_SummationOfPrimes {
 	static int testNo = 1;
-
+	
 	public long primesSum(final int upperLimit) {
-		SieveOfEratosthenes primeSieve = new SieveOfEratosthenes( upperLimit );
-		final long primeSum = LongStream.range(2, upperLimit).filter(x -> primeSieve.isPrime((int) x)).sum();
-		
-		System.out.printf("Result(%d) = %d, %n", upperLimit, primeSum);
+		final SieveOfEratosthenes primeSieve = new SieveOfEratosthenes( upperLimit );
+		final long primeSum = LongStream.range(2, upperLimit)
+			.filter(candidate -> primeSieve.isPrime((int) candidate))
+			.sum();
 		return primeSum;
 	}
 
@@ -27,12 +27,12 @@ public class P010_SummationOfPrimes {
 		testNo += 1;
 	}
 	
-	@Test(timeout = 300_000)
+	@Test(timeout = 1_000)
 	@Category(FastTest.class)
 	public void TwoMillionPrimes() {
-		// takes ~133 seconds on an i7 laptop
-		long primeSum = primesSum(2_000_000);
+		final int upperLimit = 2_000_000;
+		long primeSum = primesSum(upperLimit);
 		assertEquals( "Incorrect sum", 142913828922L, primeSum );
-
+		System.out.printf("Result(%d) = %d, %n", upperLimit, primeSum);
 	}
 }
