d545ed0321a4c18aaf10d803596d715b92ce005a
haroldalmon <HaroldAlmon@users.noreply.github.com>
2016-02-10 14:05:56 -0600

    Delete original code.

--- src/main/java/com/translationdata/p010/P010_SummationOfPrimes.java
+++ src/main/java/com/translationdata/p010/P010_SummationOfPrimes.java
@@ -8,6 +8,8 @@ import org.junit.experimental.categories.Category;
 
 import JUnitTests.FastTest;
 import JUnitTests.SlowTest;
+
+import misc.SieveOfEratosthenes;
 public class P010_SummationOfPrimes {
 	static int testNo = 1;
 
@@ -15,42 +17,10 @@ public class P010_SummationOfPrimes {
 		int primeCandidate;
 		int primeCount;
 		long primeSum = 2;
-		long primes[] = new long[200_000];
 		
-		primes[0] = 2;
-		primeCount = 1;
-		primeCandidate = 3;
 		
-		while (primeCandidate < upperLimit) {
-			boolean isPrime = true;		
-			for(int i = 0; i < primeCount; i++) {
-				if ( isRemainderOfDivisionZero(primeCandidate, primes[i]) ) {
-					isPrime = false;
-					break;
-				}
-			}
-
-			if (isPrime == true) {
-				primeSum += primeCandidate;
-				try {
-					primes[primeCount] = primeCandidate;
-				} catch (Exception e) {
-					e.printStackTrace();
-					System.out.printf("sum = %d\n", primeSum);
-				}
-				primeCount += 1;
-			}
-			primeCandidate = nextCandidate(primeCandidate);
-		}
 		System.out.printf("Result(%d) = %d, no. of primes %d%n", upperLimit, primeSum, primeCount);
 		return primeSum;
-	}
-
-	private int nextCandidate(final int candidate) {
-		return candidate + 2;
-	}
-	private boolean isRemainderOfDivisionZero (final int numerator, final long denominator) {
-		return numerator % denominator == 0;
 	}
 
 	@Before
@@ -66,19 +36,5 @@ public class P010_SummationOfPrimes {
 		long primeSum = primesSum(2_000_000);
 		assertEquals( "Incorrect sum", 142913828922L, primeSum );
 
-	}
-	
-	@Test(timeout = 500)
-	@Category(FastTest.class)
-	public void SixPrimes() {
-		long primeSum = primesSum(6);
-		assertEquals( "Incorrect sum", 10, primeSum );
-	}
-	
-	@Test(timeout = 500)
-	@Category(FastTest.class)
-	public void TenPrimes() {
-		long primeSum = primesSum(10);
-		assertEquals("Incorrect sum", 17, primeSum);
 	}
 }
